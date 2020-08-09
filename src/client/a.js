e05a05e4c4c576b796d69419942f746243a884b9
lishunyang <lishunyang@mokahr.com>
2020-08-10 01:54:30 +0800

    Add example for css-loader

---
+++ src/client/a.js
@@ -0,0 +1,6 @@
+import { hello } from './b.js'
+import './c.css'
+
+console.log('a')
+
+hello()
---
+++ src/server.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const fs = require('fs')
+const express = require('express')
+const babel = require('@babel/core')
+
+function cssLoader(filepath) {
+  return `
+  (function() {
+    var node = document.createElement('style')
+    node.innerText = \`${fs.readFileSync(filepath, 'utf-8')}\`
+    document.body.append(node)
+  }())
+  `
+}
+
+const app = express()
+
+app.use('/client/*', (req, res) => {
+  const filepath = path.join(__dirname, req.baseUrl)
+  const { code } = babel.transformFileSync(filepath, {
+    plugins: [
+      {
+        visitor: {
+          ImportDeclaration(babelPath) {
+            const extname = path.extname(babelPath.node.source.value)
+            if (extname === '.css') {
+              const cssFilepath = path.join(path.dirname(filepath), babelPath.node.source.value)
+              babelPath.replaceWithSourceString(cssLoader(cssFilepath))
+            }
+          }
+        }
+      }
+    ]
+  })
+
+  res.set('Content-Type', 'text/javascript')
+  res.status(200).send(code)
+})
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.listen(5600)
