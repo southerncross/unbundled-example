944cab8960530f21c2ae75e25d74a03221eb0953
lishunyang <lishunyang@mokahr.com>
2020-08-10 02:30:11 +0800

    node_modules to mokapack

--- src/client/a.js
+++ src/client/a.js
@@ -1,5 +1,8 @@
 const { hello } = require('./b.js')
 import './c.css'
+import lodash from 'lodash'
+
+console.log(lodash.now())
 
 console.log('a')
 
--- src/server.js
+++ src/server.js
@@ -26,6 +26,12 @@ app.use('/client/*', (req, res) => {
             if (extname === '.css') {
               const cssFilepath = path.join(path.dirname(filepath), babelPath.node.source.value)
               babelPath.replaceWithSourceString(cssLoader(cssFilepath))
+              return
+            }
+
+            const fromNodeModule = !babelPath.node.source.value.match(/^[./]/)
+            if (fromNodeModule) {
+              babelPath.node.source.value = '/__mokapack__/' + babelPath.node.source.value
             }
           }
         }
@@ -36,6 +42,41 @@ app.use('/client/*', (req, res) => {
 
   res.set('Content-Type', 'text/javascript')
   res.status(200).send(code)
+})
+
+app.use('/__mokapack__/*', (req, res) => {
+  let relativeFilepath = req.baseUrl.replace(/__mokapack__/, '')
+  let filepath = path.join(__dirname, '../node_modules', relativeFilepath)
+  const extname = path.extname(req.baseUrl)
+  if (!extname) {
+    if (fs.existsSync(filepath)) {
+      relativeFilepath += '/index.js'
+    } else {
+      relativeFilepath += '.js'
+    }
+  }
+  filepath = path.join(__dirname, '../node_modules', relativeFilepath)
+
+  const { code } = babel.transformFileSync(filepath, {
+    plugins: [
+      'transform-commonjs'
+    ]
+  })
+
+  const { code: code2 } = babel.transformSync(code, {
+    plugins: [
+      {
+        visitor: {
+          ImportDeclaration(babelPath) {
+            babelPath.node.source.value = path.join('/__mokapack__/', path.dirname(relativeFilepath), babelPath.node.source.value)
+          }
+        }
+      }
+    ]
+  })
+
+  res.set('Content-Type', 'text/javascript')
+  res.status(200).send(code2)
 })
 
 app.use(express.static(path.join(__dirname, '../public')))
